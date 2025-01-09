2d4301240cfd1ad27bc147885f2caff72694286e
kutay <kutay2408@gmail.com>
2025-01-09 23:27:36 +0100

    Created some Animations and fixes

--- app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/ui/settings/UserInfoFragment.kt
+++ app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/ui/settings/UserInfoFragment.kt
@@ -29,6 +29,55 @@ class UserInfoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val orangeDetailsFragment = view.findViewById<View>(R.id.info_card)
+        orangeDetailsFragment.setOnClickListener {
+            val navOptions = NavOptions.Builder()
+                .setEnterAnim(R.anim.fade_in)
+                .setExitAnim(R.anim.fade_out)
+                .setPopEnterAnim(R.anim.fade_in)
+                .setPopExitAnim(R.anim.fade_out)
+                .build()
+            findNavController().navigate(
+                R.id.action_UserInfoFragment_to_settingsFragment,
+                null,
+                navOptions
+            )
+        }
+
+        val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = findNavController()
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_dashboard -> { // Menüpunkt für Einstellungen
+                    val navOptions = NavOptions.Builder()
+                        .setEnterAnim(R.anim.slide_in_right)
+                        .setExitAnim(R.anim.slide_out_left)
+                        .setPopEnterAnim(R.anim.slide_in_left)
+                        .setPopExitAnim(R.anim.slide_out_right)
+                        .build()
+
+                    navController.navigate(R.id.navigation_dashboard, null, navOptions)
+                    true
+                }
+
+                R.id.navigation_home -> {
+                    // Optional: Verhindere Navigation zum Dashboard, wenn du bereits dort bist
+                    if (navController.currentDestination?.id != R.id.navigation_dashboard) {
+                        val navOptions = NavOptions.Builder()
+                            .setEnterAnim(R.anim.slide_in_right)
+                            .setExitAnim(R.anim.slide_out_left)
+                            .setPopEnterAnim(R.anim.slide_in_left)
+                            .setPopExitAnim(R.anim.slide_out_right)
+                            .build()
+                        navController.navigate(R.id.navigation_dashboard, null,navOptions)
+                    }
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 
 
