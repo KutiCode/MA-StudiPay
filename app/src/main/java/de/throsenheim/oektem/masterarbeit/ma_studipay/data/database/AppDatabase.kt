a10457a4fcfaa8bbfbc665a22f1c01de5403c9d5
kutay <kutay2408@gmail.com>
2025-01-14 19:08:28 +0100

    Created the Data access object for the Queue

--- app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/data/database/AppDatabase.kt
+++ app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/data/database/AppDatabase.kt
@@ -6,12 +6,14 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import de.throsenheim.oektem.masterarbeit.ma_studipay.data.dao.SyncQueueDao
 import de.throsenheim.oektem.masterarbeit.ma_studipay.data.dao.UserDao
 import de.throsenheim.oektem.masterarbeit.ma_studipay.data.model.User
 
 @Database(entities = [User::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
+    abstract fun syncQueueDao(): SyncQueueDao
 
     companion object {
         @Volatile
