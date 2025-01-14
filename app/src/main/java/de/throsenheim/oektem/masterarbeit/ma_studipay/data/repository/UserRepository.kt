9f71e0b5b6e778f8c7e33a8af6210480606ca8d4
kutay <kutay2408@gmail.com>
2025-01-14 22:42:39 +0100

    User-Repository wurde mit Methode zum Synchronisieren erweitert

--- app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/data/repository/UserRepository.kt
+++ app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/data/repository/UserRepository.kt
@@ -1,9 +1,11 @@
 package de.throsenheim.oektem.masterarbeit.ma_studipay.data.repository
 
+import android.util.Log
 import de.throsenheim.oektem.masterarbeit.ma_studipay.data.dao.UserDao
 import de.throsenheim.oektem.masterarbeit.ma_studipay.data.model.User
+import de.throsenheim.oektem.masterarbeit.ma_studipay.service.ApiService
 
-class UserRepository(private val userDao: UserDao) {
+class UserRepository(private val userDao: UserDao, private val apiService: ApiService) {
 
     suspend fun insertUser(user: User) {
         userDao.insertUser(user)
@@ -13,7 +15,27 @@ class UserRepository(private val userDao: UserDao) {
         return userDao.getUserByUsername(username)
     }
 
+    suspend fun getUserByMatrikelnumber(matrikelnumber: String): User? {
+        return userDao.getUserByMatrikelnumber(matrikelnumber)
 
     }
 
+    //Funktion um die Datenbank aktuell zu halten
+    suspend fun syncDatabase() {
+        try {
+            val response = apiService.getAllUsers()
+            if (response.isSuccessful) {
+                response.body()?.let { users ->
+                    for (user in users) {
+                        userDao.insertUser(user) // Aktualisiere lokale Datenbank
+                    }
+                }
+                Log.d("UserRepository", "Datenbank synchronisiert")
+            }
+        } catch (e: Exception) {
+            Log.e("UserRepository", "Fehler bei der Synchronisierung", e)
+            // Keine Verbindung oder Fehler bei der Synchronisation
+        }
+    }
+}
 
