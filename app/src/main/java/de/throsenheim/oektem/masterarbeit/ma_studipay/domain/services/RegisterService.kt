1dcff9658df1955c25b5f5e3605e1425c42aa00d
kutay <kutay2408@gmail.com>
2025-04-09 15:34:35 +0200

    Registrierung wurde extrahiert

---
+++ app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/domain/services/RegisterService.kt
@@ -0,0 +1,78 @@
+package de.throsenheim.oektem.masterarbeit.ma_studipay.domain.services
+
+import at.favre.lib.crypto.bcrypt.BCrypt
+import de.throsenheim.oektem.masterarbeit.ma_studipay.data.remote.RetrofitInstance
+import de.throsenheim.oektem.masterarbeit.ma_studipay.data.remote.request.UserRegistrationRequest
+import de.throsenheim.oektem.masterarbeit.ma_studipay.data.repository.UserRepositoryImpl
+import kotlinx.coroutines.runBlocking
+import de.throsenheim.oektem.masterarbeit.ma_studipay.domain.model.User
+
+object RegisterService {
+
+
+    fun registerService(
+        immatriculationNumber: String,
+        firstName: String,
+        lastName: String,
+        password: String,
+        userRepositoryImpl: UserRepositoryImpl
+    ): String {
+        return runBlocking {
+            val userExists =
+                userRepositoryImpl.getUserByImmatriculationNumber(immatriculationNumber) != null
+            if (userExists) {
+                return@runBlocking "Nutzer existiert bereits"
+            } else {
+                val hashedPassword = hashPassword(password)
+                val user = User(
+                    matrikelnumber = immatriculationNumber,
+                    firstName = firstName,
+                    lastName = lastName,
+                    password = hashedPassword,
+                    accountNumber = generateUniqueAccountNumber(userRepositoryImpl),
+                    balance = 0.0,
+                    securePin = "0000",
+                    bank_code = null
+                )
+                val request = UserRegistrationRequest(
+                    matrikelnumber = immatriculationNumber,
+                    firstName = firstName,
+                    lastName = lastName,
+                    password = hashedPassword,
+                    accountNumber = user.accountNumber,
+                    balance = user.balance,
+                    securePin = user.securePin
+                )
+                try {
+                    val response = RetrofitInstance.api.registerUser(request)
+                    if (response.isSuccessful) {
+                        userRepositoryImpl.insertUser(user)
+                        userRepositoryImpl.syncDatabase()
+
+                    } else {
+                        return@runBlocking "Registrierung beim Backend fehlgeschlagen"
+
+                    }
+                } catch (e: Exception) {
+                    return@runBlocking "Fehler: ${e.message}"
+
+                }
+                return@runBlocking "User erfolgreich registriert"
+            }
+
+        }
+    }
+
+
+    private fun hashPassword(password: String): String {
+        return BCrypt.withDefaults().hashToString(12, password.toCharArray())
+    }
+
+    private suspend fun generateUniqueAccountNumber(userRepositoryImpl: UserRepositoryImpl): String {
+        var kontonummer: String
+        do {
+            kontonummer = (100000..999999).random().toString()
+        } while (userRepositoryImpl.userDao.getAllUsers().any { it.accountNumber == kontonummer })
+        return kontonummer
+    }
+}
--- app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/ui/viewmodel/register/RegisterViewModel.kt
+++ app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/ui/viewmodel/register/RegisterViewModel.kt
@@ -3,13 +3,12 @@ package de.throsenheim.oektem.masterarbeit.ma_studipay.ui.viewmodel.register
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import at.favre.lib.crypto.bcrypt.BCrypt
+
 import de.throsenheim.oektem.masterarbeit.ma_studipay.data.repository.UserRepositoryImpl
-import de.throsenheim.oektem.masterarbeit.ma_studipay.domain.model.User
-import de.throsenheim.oektem.masterarbeit.ma_studipay.data.remote.RetrofitInstance
-import de.throsenheim.oektem.masterarbeit.ma_studipay.data.remote.request.UserRegistrationRequest
-import kotlinx.coroutines.launch
+
+import de.throsenheim.oektem.masterarbeit.ma_studipay.domain.services.RegisterService
+
+
 
 
 class RegisterViewModel(private val userRepositoryImpl: UserRepositoryImpl) : ViewModel() {
@@ -39,59 +38,18 @@ class RegisterViewModel(private val userRepositoryImpl: UserRepositoryImpl) : Vi
                 "Das Passwort muss mindestens einen Großbuchstaben und eine Zahl enthalten"
             return
         }
-
-        viewModelScope.launch {
-            val userExists = userRepositoryImpl.getUserByImmatriculationNumber(matrikelnummer) != null
-            if (userExists) {
-                _errorMessage.value = "Matrikelnummer existiert bereits"
-            } else {
-                val hashedPassword = hashPassword(password)
-                val user = User(
-                    matrikelnumber = matrikelnummer,
-                    firstName = firstName,
-                    lastName = lastName,
-                    password = hashedPassword,
-                    accountNumber = generateUniqueKontonummer(),
-                    balance = 0.0,
-                    securePin = "0000",
-                    bank_code = null
-                )
-
-                // Sende den neuen User an das Backend
-                val request = UserRegistrationRequest(
-                    matrikelnumber = matrikelnummer,
-                    firstName = firstName,
-                    lastName = lastName,
-                    password = hashedPassword,
-                    accountNumber = user.accountNumber,
-                    balance = user.balance,
-                    securePin = user.securePin
-                )
-                try {
-                    val response = RetrofitInstance.api.registerUser(request)
-                    if (response.isSuccessful) {
-                        userRepositoryImpl.insertUser(user)
-                        userRepositoryImpl.syncDatabase()
-                        _registrationResult.value = true
-                    } else {
-                        _errorMessage.value = "Registrierung beim Backend fehlgeschlagen"
-                    }
-                } catch (e: Exception) {
-                    _errorMessage.value = "Fehler: ${e.message}"
-                }
-            }
+        val registerResponse = RegisterService.registerService(
+            matrikelnummer,
+            firstName,
+            lastName,
+            password,
+            userRepositoryImpl
+        )
+        if (registerResponse == "User erfolgreich registriert") {
+            _registrationResult.value = true
+        } else {
+            _errorMessage.value = registerResponse
         }
     }
 
-
-    fun hashPassword(password: String): String {
-        return BCrypt.withDefaults().hashToString(12, password.toCharArray())
-    }
-    private suspend fun generateUniqueKontonummer(): String {
-        var kontonummer: String
-        do {
-            kontonummer = (100000..999999).random().toString()
-        } while (userRepositoryImpl.userDao.getAllUsers().any { it.accountNumber == kontonummer })
-        return kontonummer
-    }
 }
