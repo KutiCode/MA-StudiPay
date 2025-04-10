75e60804de72e2139df7c6d74af4f3e76d449a0e
kutay <kutay2408@gmail.com>
2025-04-10 19:42:19 +0200

    Neue Helper Klasse

---
+++ app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/domain/utility/NavigationHelper.kt
@@ -0,0 +1,76 @@
+package de.throsenheim.oektem.masterarbeit.ma_studipay.domain.utility
+
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import de.throsenheim.oektem.masterarbeit.ma_studipay.R
+
+object NavigationHelper {
+
+
+    fun buildSlideNavOptions(): NavOptions {
+        return NavOptions.Builder()
+            .setEnterAnim(R.anim.slide_in_right)
+            .setExitAnim(R.anim.slide_out_left)
+            .setPopEnterAnim(R.anim.slide_in_left)
+            .setPopExitAnim(R.anim.slide_out_right)
+            .build()
+    }
+
+
+    fun buildFadeNavOptions(): NavOptions {
+        return NavOptions.Builder()
+            .setEnterAnim(R.anim.fade_in)
+            .setExitAnim(R.anim.fade_out)
+            .setPopEnterAnim(R.anim.fade_in)
+            .setPopExitAnim(R.anim.fade_out)
+            .build()
+    }
+
+
+    fun buildNavOptions(
+        enterAnim: Int,
+        exitAnim: Int,
+        popEnterAnim: Int = enterAnim,
+        popExitAnim: Int = exitAnim
+    ): NavOptions {
+        return NavOptions.Builder()
+            .setEnterAnim(enterAnim)
+            .setExitAnim(exitAnim)
+            .setPopEnterAnim(popEnterAnim)
+            .setPopExitAnim(popExitAnim)
+            .build()
+    }
+
+
+    fun setupBottomNavigation(
+        bottomNavigationView: BottomNavigationView,
+        navController: NavController
+    ) {
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.dashboardFragment -> {
+                    navController.navigate(
+                        R.id.dashboardFragment,
+                        null,
+                        buildSlideNavOptions()
+                    )
+                    true
+                }
+
+                R.id.navigation_home -> {
+                    if (navController.currentDestination?.id != R.id.dashboardFragment) {
+                        navController.navigate(
+                            R.id.dashboardFragment,
+                            null,
+                            buildSlideNavOptions()
+                        )
+                    }
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+}
