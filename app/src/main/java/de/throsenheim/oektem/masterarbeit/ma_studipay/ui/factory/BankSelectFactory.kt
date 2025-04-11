7c61984e79d9f13e9f719b67d50ca257c632b844
kutay <kutay2408@gmail.com>
2025-04-11 02:11:39 +0200

    Kommentare eingefügt

--- app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/ui/factory/BankSelectFactory.kt
+++ app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/ui/factory/BankSelectFactory.kt
@@ -6,15 +6,29 @@ import androidx.lifecycle.ViewModelProvider
 import de.throsenheim.oektem.masterarbeit.ma_studipay.data.repository.UserRepositoryImpl
 import de.throsenheim.oektem.masterarbeit.ma_studipay.ui.viewmodel.bank.BankSelectViewModel
 
+// Factory to create instances of BankSelectViewModel with the required dependencies.
 class BankSelectFactory(
+    // Application context used for operations like accessing SharedPreferences.
     private val context: Context,
+    // The repository needed to perform user-related operations and assign banks.
     private val userRepositoryImpl: UserRepositoryImpl
 ) : ViewModelProvider.Factory {
+
+    /**
+     * Creates a new instance of the requested ViewModel.
+     *
+     * @param modelClass the class type of the requested ViewModel.
+     * @return an instance of BankSelectViewModel.
+     * @throws IllegalArgumentException if the requested ViewModel is not BankSelectViewModel.
+     */
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        // Check if the modelClass is assignable from BankSelectViewModel.
         if (modelClass.isAssignableFrom(BankSelectViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
+            // Return a new instance of BankSelectViewModel with the provided context and repository.
             return BankSelectViewModel(context, userRepositoryImpl) as T
         }
+        // Throw an exception if the ViewModel class is unknown.
         throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
--- app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/ui/factory/RegisterFactory.kt
+++ app/src/main/java/de/throsenheim/oektem/masterarbeit/ma_studipay/ui/factory/RegisterFactory.kt
@@ -5,14 +5,27 @@ import androidx.lifecycle.ViewModelProvider
 import de.throsenheim.oektem.masterarbeit.ma_studipay.data.repository.UserRepositoryImpl
 import de.throsenheim.oektem.masterarbeit.ma_studipay.ui.viewmodel.register.RegisterViewModel
 
+// Factory for creating instances of RegisterViewModel with the required UserRepositoryImpl dependency.
 class RegisterFactory(
+    // The repository instance needed to perform registration operations in the ViewModel.
     private val userRepositoryImpl: UserRepositoryImpl
 ) : ViewModelProvider.Factory {
 
+    /**
+     * Creates a new instance of the requested ViewModel.
+     *
+     * @param modelClass The class of the ViewModel that is requested.
+     * @return An instance of RegisterViewModel.
+     * @throws IllegalArgumentException if the ViewModel class is not supported by this factory.
+     */
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        // Check if the requested ViewModel is of type RegisterViewModel.
         if (modelClass.isAssignableFrom(RegisterViewModel::class.java)) {
+            // Return a new instance of RegisterViewModel with the provided UserRepositoryImpl dependency.
+            @Suppress("UNCHECKED_CAST")
             return RegisterViewModel(userRepositoryImpl) as T
         }
+        // Throw an exception if an unsupported ViewModel class is requested.
         throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
